780177202405da35b267b11cf25d802a51827b6d
Stubaccus <stubaccus.jeu@gmail.com>
2025-02-20 15:43:02 +0100

    creation de la bd
    creation de 3 tables qu'on utilisera pour la sae

---
+++ database.sql
@@ -0,0 +1,28 @@
+CREATE TABLE IF NOT EXISTS games(                                                                                                           -- tables des parties
+    id INTEGER PRIMARY KEY AUTOINCREMENT,                                                                                                   -- id unique de la partie
+    name TEXT NOT NULL,                                                                                                                     -- nom de la partie 
+    status TEXT CHECK(status IN ('waiting', 'play', 'over')) DEFAULT 'waiting',                                                             -- statue de la partie
+    player1 TEXT NOT NULL,                                                                                                                  -- nom du joueur 1
+    player1_role TEXT CHECK(player1_role IN ('human', 'AI')) NOT NULL,                                                                      -- role du joueur 1
+    player2 TEXT,                                                                                                                           -- nom du joueur 2
+    player2_role TEXT CHECK(player2_role IN ('human', 'AI')),                                                                               -- role du joueur 2
+    board TEXT NOT NULL DEFAULT '[ [0,0,0,0,0,0,0], [0,0,0,0,0,0,0], [0,0,0,0,0,0,0], [0,0,0,0,0,0,0], [0,0,0,0,0,0,0], [0,0,0,0,0,0,0] ]', -- plateau de jeu
+    player_turn INTEGER DEFAULT 1                                                                                                          -- tour du joueur
+);
+
+CREATE TABLE IF NOT EXISTS moves (                                                                                                          -- table des mouvements (la ou sont stocké les coups joué)
+    id INTEGER PRIMARY KEY AUTOINCREMENT,                                                                                                   -- id unique du mouvement
+    game_id INTEGER,                                                                                                                        -- id de la partie en cours 
+    player INTEGER,                                                                                                                         -- id du joueur ayant joué
+    column INTEGER,                                                                                                                         -- colonne ou a mis un pion
+    FOREIGN KEY (game_id) REFERENCES games(id)                                                                                              -- lien avec la table games 
+);
+
+
+CREATE TABLE IF NOT EXISTS players (                                                                                                        -- table des joueurs
+    id INTEGER PRIMARY KEY AUTOINCREMENT,                                                                                                   -- id unique du joueur
+    game_id INTEGER,                                                                                                                        -- id de la partie en cours
+    player_name TEXT,                                                                                                                       -- nom du joueur
+    private_key TEXT,                                                                                                                       -- clé privée du joueur
+    FOREIGN KEY (game_id) REFERENCES games(id)                                                                                              -- lien avec la table games
+);
