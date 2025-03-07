df061b4cef90f1b3993542493317026f03ba15ee
Stubaccus <stubaccus.jeu@gmail.com>
2025-03-07 14:57:43 +0100

    edit database
    il manquait des choses

--- database.sql
+++ database.sql
@@ -1,13 +1,17 @@
-CREATE TABLE IF NOT EXISTS games(                                                                                                           -- tables des parties
+CREATE TABLE IF NOT EXISTS games(                                                                                                          
     id INTEGER PRIMARY KEY AUTOINCREMENT,                                                                                                   -- id unique de la partie
     name TEXT NOT NULL,                                                                                                                     -- nom de la partie 
+    game_path TEXT,
     status TEXT CHECK(status IN ('waiting', 'play', 'over')) DEFAULT 'waiting',                                                             -- statue de la partie
     player1 TEXT NOT NULL,                                                                                                                  -- nom du joueur 1
     player1_role TEXT CHECK(player1_role IN ('human', 'AI')) NOT NULL,                                                                      -- role du joueur 1
+    player1_path TEXT,
     player2 TEXT,                                                                                                                           -- nom du joueur 2
     player2_role TEXT CHECK(player2_role IN ('human', 'AI')),                                                                               -- role du joueur 2
-    board TEXT NOT NULL DEFAULT '[ [0,0,0,0,0,0,0], [0,0,0,0,0,0,0], [0,0,0,0,0,0,0], [0,0,0,0,0,0,0], [0,0,0,0,0,0,0], [0,0,0,0,0,0,0] ]', -- plateau de jeu
-    player_turn INTEGER DEFAULT 1                                                                                                           -- tour du joueur
+    player2_path TEXT,
+    board TEXT NOT NULL DEFAULT '[ [0,0,0,0,0,0,0], [0,0,0,0,0,0,0], [0,0,0,0,0,0,0], [0,0,0,0,0,0,0], [0,0,0,0,0,0,0], [0,0,0,0,0,0,0] ]',  -- plateau de jeu
+    player_turn INTEGER DEFAULT 1 ,                                                                                                          -- tour du joueur
+    last_move INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS moves (                                                                                                          -- table des mouvements (la ou sont stocké les coups joué)
